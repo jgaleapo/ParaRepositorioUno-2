97e61b1233d9760fd46a247f3df2e897563f9453
Haruichi <Haruichi@EQUIPO11>
2015-02-12 13:41:14 +0100

    Herencia abstracta e interfaz

---
+++ Prueba/src/C03Clase.java
@@ -0,0 +1,16 @@
+
+public class C03Clase extends C02Abstracta implements I00Interface {
+
+	@Override
+	public void adiosito() {
+	System.out.println("Soy un adiosito de la clase abstracta heredado.");
+
+	}
+
+	@Override
+	public void saluditos() {
+		System.out.println("Soy un saludito de la interface heredada");
+		
+	}
+
+}
